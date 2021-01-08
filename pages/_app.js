1bf09fb5716e5fb589e9d53c41832f33927c1633
Francisco Javier Arceo <arceofrancisco@gmail.com>
2021-01-08 00:35:31 -0500

    updated app file and removed stuff...looks like main file is the issue though

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,6 @@ import { ThemeProvider } from "next-themes";
 
 import "@assets/main.css";
 
-// import "typeface-open-sans";
-// import "typeface-merriweather";
-
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" enableSystem={true} attribute="class">
