9fa68dc5392faff9b51ec58e63c50424f1c5df14
Francisco Javier Arceo <arceofrancisco@gmail.com>
2021-01-11 21:08:00 -0500

    updated config to adjust sitemap

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 const optimizedImages = require("next-optimized-images");
 
 module.exports = optimizedImages;
+
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/sitemap.xml',
+          headers: [
+            {
+              key: 'Content-type',
+              value: 'application/xml',
+            }
+          ],
+        },
+      ]
+    },
+  }
+  
